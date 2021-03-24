27d7ee16dc8477a3586e8610c5b00e47d4a1cff9
Klaudia Barańska <klaudiabaranska11@gmail.com>
2021-03-24 14:29:51 +0100

    wczytywanie danych

--- PwJP_TextMining.py
+++ PwJP_TextMining.py
@@ -6,3 +6,9 @@ Created on Wed Mar 24 14:12:27 2021
 """
 
 # kod
+import pandas
+pandas.read_csv
+df = pandas.read_csv('women_clothing_review.csv')
+list_reviews = df.values.tolist()
+
+list_reviews_text=df['review_text']
